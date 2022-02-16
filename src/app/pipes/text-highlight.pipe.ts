49d3b8f1b9ed3bda1688172cc41344ec72ade602
Yedigaryan <davit.yedigaryan.1993@gmail.com>
2022-02-16 17:28:14 +0400

    updates to deploy to heroku

--- src/app/pipes/text-highlight.pipe.ts
+++ src/app/pipes/text-highlight.pipe.ts
@@ -5,24 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TextHighlightPipe implements PipeTransform {
 
+  private static escapeRegExp(string: string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+  }
+
   transform(value: any, args: string): any {
     if (!args) {
       return value;
     }
     const div = document.createElement('div')
-    const argsArray = Array.from(new Set(args.trim().split(' ')));
-    argsArray.map((args) => {
-      if (args !== ' ') {
-        const regex = new RegExp(args, 'gi');
-        const match = value.match(regex)
-        if (!match) {
-          return value
-        }
-        div.innerHTML = value;
-        value = div.innerText;
-       value = value.replace(regex, `<span class='highlight'>${match[0]}</span>`);
-      }
-    })
+    const argsArray = Array.from(new Set(args.trim().split(' '))).filter(Boolean);
+    const regex = new RegExp(
+      `(${argsArray.map((value) => `(${TextHighlightPipe.escapeRegExp(value)})`).join('|')})`,
+      'gi',
+    );
+
+    div.innerHTML = value;
+    value = div.innerText;
+    value = value.replace(regex, '<span class=\'highlight\'>$1</span>');
+
     return value;
   }
 }
